20a35d342b9e15265bf8ed468f87f1768b216e44
hankai <1245906870@qq.com>
2019-07-25 00:11:59 +0800

    提交

--- auth/src/main/java/com/khan/auth/config/WebSecurityConfig.java
+++ auth/src/main/java/com/khan/auth/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .permitAll()
                 .and()
-                .httpBasic()
+                 .httpBasic()
         ;
     }
 
