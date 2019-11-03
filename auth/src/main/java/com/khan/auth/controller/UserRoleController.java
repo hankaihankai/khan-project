aaec0e9c63dba0e38980be297a44ce4ca784326e
hankai <1245906870@qq.com>
2019-11-03 18:31:25 +0800

    提交

--- auth/src/main/java/com/khan/auth/controller/UserRoleController.java
+++ auth/src/main/java/com/khan/auth/controller/UserRoleController.java
@@ -17,4 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user-role")
 public class UserRoleController {
 
+    public static void main(String[] args) {
+
+    }
 }
